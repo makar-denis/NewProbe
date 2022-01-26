feb254a56bd4254b7109948daff3b2e5aa4c3111
Денис <makar_denis@tut.by>
2022-01-26 18:51:26 +0300

    Создал второй коммит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Создан реппозиторий");
         System.out.println("Создал первый коммит");
+        System.out.println("Создал второй коммит");
     }
 }
