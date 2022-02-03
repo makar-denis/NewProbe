f7e4e90c53c1894c32db3ad18d0398984ab801cf
Денис <makar_denis@tut.by>
2022-02-03 19:25:10 +0300

    Еще один комит в основной ветке

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Создан реппозиторий");
         System.out.println("Создал первый коммит");
         System.out.println("Удалил второй коммит");
+        System.out.println("еще один коммит в основном потоке");
+
     }
 }
